7068cdd1deaabf585099e57af74cfb12f3cd458a
Iván <ivanyn9@gmail.com>
2020-02-08 13:41:29 +0100

    Frontend start form

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <StartForm/>
+    <Timer/>
+  </div>
+</template>
+
+<script>
+import StartForm from './components/StartForm.vue'
+import Timer from './components/Timer.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      task: null,
+      duration: null,
+      shortBreakDuration: null,
+      longBreakDuration: null,
+      longBreakAfter: null
+    }
+  },
+  components: {
+    StartForm,
+    Timer
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/StartForm.vue
@@ -0,0 +1,59 @@
+<template>
+  <form @submit.prevent="submit()">
+    <h1>Start a pomodoro</h1>
+    <div class="form-group">
+      <label for="task">Task</label>
+      <input type="text" id="task" class="form-control" v-model="task" required>
+    </div>
+    <div class="form-group">
+      <label for="duration">Pomodoro duration</label>
+      <input type="number" id="duration" class="form-control" min="1" v-model="duration">
+      <small class="form-text text-muted">In minutes</small>
+    </div>
+    <div class="form-group">
+      <label for="short-break">Short break duration</label>
+      <input type="number" id="short-break" class="form-control" min="1" v-model="shortBreakDuration">
+      <small class="form-text text-muted">In minutes</small>
+    </div>
+    <div class="form-group">
+      <label for="long-break">Long break duration</label>
+      <input type="number" id="long-break" class="form-control" min="1" v-model="longBreakDuration">
+      <small class="form-text text-muted">In minutes</small>
+    </div>
+    <div class="form-group">
+      <label for="long-break">Long break after</label>
+      <input type="number" id="long-break" class="form-control" min="1" v-model="longBreakAfter">
+      <small class="form-text text-muted">In pomodoros</small>
+    </div>
+    <button type="submit" class="btn btn-primary">Start</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      task: '',
+      duration: 25,
+      shortBreakDuration: 5,
+      longBreakDuration: 15,
+      longBreakAfter: 4
+    }
+  },
+  methods: {
+    submit() {
+      this.$emit('submit', {
+        task: this.task,
+        duration: this.duration,
+        shortBreakDuration: this.shortBreakDuration,
+        longBreakDuration: this.longBreakDuration,
+        longBreakAfter: this.longBreakAfter,
+      })
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
